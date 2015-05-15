05b1f39574dc9300002f8c930461a9673f1d6741
TaisceCheol <piaras.hoban@itma.ie>
2015-05-15 12:42:36 +0100

    String processing rules added

--- processor.py
+++ processor.py
@@ -1,24 +1,45 @@
 # -*- coding: ISO-8859-1 -*-
 import os
 import xml.etree.cElementTree as ET
+from  collections import OrderedDict
 
+def rules(field,data):
+	if field == 'Title' or field == 'AdditionalTitle':
+		return ".  ".join(data)
+	elif field == 'ArchiveLocation':
+		return " -- ".join(data)
+	elif field == 'PhysicalDescription':
+		return " ; ".join(data)
+
+def processRecord(record):
+	for item in record.iteritems():
+		if len(item[-1]) > 1:
+			record[item[0]] = [rules(*item)]
+			
 def parseXML(tree):
 	records = []
 	fields = ["REFNO","Title","AdditionalTitle","PhysicalDescription","ArchiveLocation"]	
 	idstring = "{http://www.inmagic.com/webpublisher/query}"
 	for record in tree.iter(tag=idstring+"Record"):
 		details = record.getchildren()
-		dc = {}
+		dc = OrderedDict()
 		for i in details:
 			stag = i.tag.replace(idstring,'')
 			if stag in fields:
-				dc[stag] = i.text
+				if i.text:
+					if stag in dc.keys():
+						dc[stag].append(i.text)
+					else:
+						dc[stag] = [i.text]
 		records.append(dc)
 	return records
 
 
-tree = ET.ElementTree(file=source)
+path = 'data/'
+files = os.listdir(path)
 
-data =	parseXML(tree)
-
-for record in data:
+for f in files:
+	tree = ET.ElementTree(file=os.path.join(path,f))
+	data =	parseXML(tree)
+	for r in data:
+		processRecord(r)
